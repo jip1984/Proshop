1299ae248f62fb3036475d91dd3ed150434b2369
jon <jonprosser84@gmail.com>
2021-06-01 19:42:41 +0100

    Ecommerce MERN stack

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
     <>
 
-      <h1>Welcome to Proshop</h1>
+      <Header />
 
+
+      <main>
+        <h1>Welcome to Proshop</h1>
+      </main>
+      <Footer />
     </>
   );
 }
